07255da4a3e4c0302771e1e7bd6e6fe3294ab0e5
kacn1z-del <kacn1z@gmail.com>
2026-09-26 14:00:42 -0600

    Add AdminPanel and conditional rendering

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import AdminPanel from './AdminPanel.jsx'
+
+function Raiz() {
+  const esAdmin = window.location.hash.startsWith('#admin')
+  return esAdmin ? <AdminPanel /> : <App />
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Raiz />
   </StrictMode>,
 )
